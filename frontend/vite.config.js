c4c3085ae0f79f94e4d84953e0b0c8cfe31b0020
gutscas <santhoshkalangiyam@gmail.com>
2025-11-27 15:15:41 +0530

    santo6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/santo/',   // 👈 very important for subpath deployment
+  build: {
+    outDir: 'build', // same folder name you used in nginx
+  },
 })
